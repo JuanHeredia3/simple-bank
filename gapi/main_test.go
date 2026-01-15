18f9c4544a6baaea235ca14eec25917e4b2e90c9
JuanHeredia3 <juanih67@hotmail.com>
2026-01-15 08:04:00 -0300

    64. Implement email verification API in Go

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "github.com/JuanHeredia3/simple-bank/db/sqlc"
+	"github.com/JuanHeredia3/simple-bank/token"
 	"github.com/JuanHeredia3/simple-bank/util"
 	"github.com/JuanHeredia3/simple-bank/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -19,4 +23,17 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	bearerToken := fmt.Sprintf("Bearer %s", accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+
+	return metadata.NewIncomingContext(context.Background(), md)
 }
